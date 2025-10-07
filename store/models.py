94bf09b7f1f7deb28631641858f48e6aab30f5b2
deffin05 <dm.melnyk05@gmail.com>
2025-10-07 15:05:07 +0300

    Create models and migration

--- store/models.py
+++ store/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.IntegerField()
+    author = models.ForeignKey('Author', on_delete=models.PROTECT)
+    publisher = models.ForeignKey('Publisher', on_delete=models.PROTECT)
+    pages = models.IntegerField()
+    year = models.IntegerField()
+    available = models.BooleanField()
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=100)
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
