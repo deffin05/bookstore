f6c7f1c97ffc3f43c3a5386b648b92a041b1db04
deffin05 <dm.melnyk05@gmail.com>
2025-10-14 20:47:21 +0300

    Add object permissions

--- auth/permissions.py
+++ auth/permissions.py
@@ -5,3 +5,8 @@ class IsStaffOrReadOnly(permissions.BasePermission):
         if request.method in permissions.SAFE_METHODS:
             return True
         return request.user.is_staff
+
+    def has_object_permission(self, request, view, obj):
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return request.user.is_staff
