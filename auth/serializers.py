89ef69ba864c79222ba23fffb5d0a56e1284ae4e
deffin05 <dm.melnyk05@gmail.com>
2025-10-14 16:32:00 +0300

    Add more validations

--- auth/serializers.py
+++ auth/serializers.py
@@ -4,14 +4,15 @@ from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
-    first_name = serializers.CharField(required=False)
-    last_name = serializers.CharField(required=False)
-    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
-    password = serializers.CharField(style={'input_type': 'password'}, write_only=True, min_length=8)
+    first_name = serializers.CharField(required=False, max_length=100)
+    last_name = serializers.CharField(required=False, max_length=100)
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())],
+                                   max_length=100)
+    password = serializers.CharField(style={'input_type': 'password'}, write_only=True, min_length=8, max_length=32)
 
     class Meta:
         model = User
-        fields = ['id', 'username','first_name', 'last_name', 'email', 'password']
+        fields = ['id', 'username', 'first_name', 'last_name', 'email', 'password']
 
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
