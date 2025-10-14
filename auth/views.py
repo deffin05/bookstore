e0fdad3c31411a8a4aa00254b42ce8577bc7e2a9
deffin05 <dm.melnyk05@gmail.com>
2025-10-14 20:48:19 +0300

    Implement UserDetail view

--- auth/views.py
+++ auth/views.py
@@ -1,4 +1,7 @@
+from django.contrib.auth.models import User
+from django.http import Http404
 from rest_framework import status
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -8,6 +11,7 @@ from auth.serializers import UserSerializer
 # Create your views here.
 
 class UserList(APIView):
+    permission_classes = (AllowAny,)
     def post(self, request):
         serializer = UserSerializer(data=request.data)
         if serializer.is_valid():
@@ -17,4 +21,28 @@ class UserList(APIView):
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 class UserDetail(APIView):
-    pass
+    def get_object(self, pk):
+        try:
+            return User.objects.get(pk=pk)
+        except User.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        if pk == 0:
+            pk = request.user.id
+        user = self.get_object(pk)
+        serializer = UserSerializer(user)
+        return Response(serializer.data)
+
+    def patch(self, request, pk):
+        user = self.get_object(pk)
+        serializer = UserSerializer(user, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        user = self.get_object(pk)
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
