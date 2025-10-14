5ddb5f5b593144d492fed7a391d2ef9b7be4796d
deffin05 <dm.melnyk05@gmail.com>
2025-10-14 20:47:46 +0300

    Add endpoint to verify token

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from auth.views import UserList, UserDetail
 from store.views import BookList, AuthorList, PublisherList, BookDetail, AuthorDetail, PublisherDetail
@@ -34,4 +34,5 @@ urlpatterns = [
     path('users/<int:pk>', UserDetail.as_view()),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/token/verify/", TokenVerifyView.as_view(), name="token_verify"),
 ]
