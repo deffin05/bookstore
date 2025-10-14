4cb6bfd8ea45fb2abccedaedd4c641b3717095e6
deffin05 <dm.melnyk05@gmail.com>
2025-10-14 16:10:24 +0300

    Make user emails unique

--- auth/serializers.py
+++ auth/serializers.py
@@ -1,12 +1,14 @@
 from django.contrib.auth.models import User
 from rest_framework import serializers
+from rest_framework.validators import UniqueValidator
 
 
 class UserSerializer(serializers.ModelSerializer):
     first_name = serializers.CharField(required=False)
     last_name = serializers.CharField(required=False)
-    email = serializers.EmailField(required=True)
+    email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
     password = serializers.CharField(style={'input_type': 'password'}, write_only=True, min_length=8)
+
     class Meta:
         model = User
         fields = ['id', 'username','first_name', 'last_name', 'email', 'password']
@@ -14,4 +16,3 @@ class UserSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         user = User.objects.create_user(**validated_data)
         return user
-
